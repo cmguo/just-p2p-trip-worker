54b7f09e70a36bee57c5f8b556890c602374be98
isxxguo <isxxguo@pptv.com>
2016-01-11 10:32:31 +0800

    注册外部配置 register_ext_config
    Change-Id: Ie51df05ca42cf29ce5fe823d0b587768d6b30c82

--- WorkerModule.cpp
+++ WorkerModule.cpp
@@ -27,10 +27,32 @@ namespace trip
     namespace worker
     {
 
+        static void trip_set_config(
+            std::string const & sec, 
+            std::string const & key, 
+            std::string const & value)
+        {
+            TRIP_SetConfig(NULL, sec.c_str(), key.c_str(), value.c_str());
+        } 
+
+        static void trip_get_config(
+            std::string const & sec, 
+            std::string const & key, 
+            std::string & value)
+        {
+            char const * v = TRIP_GetConfig(NULL, sec.c_str(), key.c_str());
+            if (v)
+                value = v;
+        } 
+
         WorkerModule::WorkerModule(
             util::daemon::Daemon & daemon)
             : util::daemon::ModuleBase<WorkerModule>(daemon, "WorkerModule")
         {
+            config().register_ext_config(
+                "trip", 
+                trip_set_config, 
+                trip_get_config);
         }
 
         WorkerModule::~WorkerModule()
